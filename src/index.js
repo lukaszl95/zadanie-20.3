8e14ea4fe3da2b3f799fe12843165e6cd87ef637
Łukasz Łućko <lukaszl95@o2.pl>
2018-03-18 20:37:08 +0100

    Zadanie-20.4

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store';
 import DevTools from './DevTools';
+import routes from './routes';
 
 render(
     <Provider store={store}>
-        <div>
-        	<h1>Inicjalizacja projektu</h1>
+         <Router history={hashHistory} routes={routes}>
         	<DevTools />
-        </div>
+        </Router>
     </Provider>,
     document.getElementById('root')
 );
