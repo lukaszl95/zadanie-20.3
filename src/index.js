31bea0a83ffdb41727c536a50346d03c36b53684
Łukasz Łućko <lukaszl95@o2.pl>
2018-03-18 20:44:45 +0100

    Zadanie-20.3

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store';
 import DevTools from './DevTools';
-import routes from './routes';
 
 render(
     <Provider store={store}>
-         <Router history={hashHistory} routes={routes}>
+        <div>
+        	<h1>Inicjalizacja projektu</h1>
         	<DevTools />
-        </Router>
+        </div>
     </Provider>,
     document.getElementById('root')
 );
--- src/presentational/not-found.component.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const NotFound = () => (
-    <h1>Error 404...podana strona nie istnieje</h1>
-);
-
-export default NotFound;
